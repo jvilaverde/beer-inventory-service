8d51a3d5986fe21bb5ce9b1533d0114f857daf4c
Joao <blackened50@gmail.com>
2020-01-27 18:33:02 +0000

    Added local mysql

--- src/main/java/com/jvilaverde/beers/beerinventoryservice/domain/BeerInventory.java
+++ src/main/java/com/jvilaverde/beers/beerinventoryservice/domain/BeerInventory.java
@@ -4,7 +4,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -15,16 +17,25 @@ import java.util.UUID;
 @Entity
 public class BeerInventory extends BaseEntity {
 
+    @Type(type="org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false )
+    private UUID beerId;
+
+    private String upc;
+
+    private Integer quantityOnHand = 0;
+
     @Builder
-    public BeerInventory(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, UUID beerId,
-                         String upc, Integer quantityOnHand) {
+    public BeerInventory(UUID id,
+                         Long version,
+                         Timestamp createdDate,
+                         Timestamp lastModifiedDate,
+                         UUID beerId,
+                         String upc,
+                         Integer quantityOnHand) {
         super(id, version, createdDate, lastModifiedDate);
         this.beerId = beerId;
         this.upc = upc;
         this.quantityOnHand = quantityOnHand;
     }
-
-    private UUID beerId;
-    private String upc;
-    private Integer quantityOnHand = 0;
 }
